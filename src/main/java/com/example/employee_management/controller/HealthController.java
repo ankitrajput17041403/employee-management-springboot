a0818d2579b340250f081a609882bdee60214b8d
Ankit Rajput <ankitrajput17041403@gmail.com>
2026-01-21 18:17:59 +0400

    Add health endpoint and fix controller configuration

---
+++ src/main/java/com/example/employee_management/controller/HealthController.java
@@ -0,0 +1,19 @@
+package com.example.employee_management.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+
+    public String health() {
+        System.out.println("Employe..");
+        return "API is running";
+    }
+}
+
+
